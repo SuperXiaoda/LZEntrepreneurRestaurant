511ef8b745b4c9107815d640f381e540155748bf
梁宏达 <1401432577@qq.com>
2018-01-22 13:55:29 +0800

    引入第三方库文件

--- app/src/main/java/cn/com/lz/entrepreneur/restaurant/App.java
+++ app/src/main/java/cn/com/lz/entrepreneur/restaurant/App.java
@@ -1,9 +1,28 @@
 package cn.com.lz.entrepreneur.restaurant;
 
+import android.app.Application;
+
 /**
  * Description:App
  * author: lhd
  * Date: 2018/1/22
  */
-public class App {
+public class App extends Application {
+
+    private static App mInstance;
+
+    /**
+     * 获取Application实例
+     *
+     * @return Application实例
+     */
+    public static App getInstance() {
+        return mInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
 }
