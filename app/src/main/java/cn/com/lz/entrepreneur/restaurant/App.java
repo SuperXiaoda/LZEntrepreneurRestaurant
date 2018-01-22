4b2c54d3515e8ea77d05ac287519e84c45ea5930
梁宏达 <1401432577@qq.com>
2018-01-22 11:52:06 +0800

    Initial commit

---
+++ app/src/main/java/cn/com/lz/entrepreneur/restaurant/App.java
@@ -0,0 +1,9 @@
+package cn.com.lz.entrepreneur.restaurant;
+
+/**
+ * Description:App
+ * author: lhd
+ * Date: 2018/1/22
+ */
+public class App {
+}
---
+++ app/src/main/java/cn/com/lz/entrepreneur/restaurant/activity/MainActivity.java
@@ -0,0 +1,20 @@
+package cn.com.lz.entrepreneur.restaurant.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.com.lz.entrepreneur.restaurant.R;
+
+/**
+ * Description:MainActivity
+ * author: lhd
+ * Date: 2018/1/22
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
